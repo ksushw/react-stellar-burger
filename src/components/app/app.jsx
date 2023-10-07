f5ee670b24340af03b3a415f16895f4474f13f53
ksushw <xenianevarenyh@yandex.ru>
2023-10-07 18:51:26 +0500

    delete useless variables

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,14 +3,9 @@ import styles from "./app.module.css";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { getIngredients } from "../../services/actions/ingredients";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import AppHeader from "../AppHeader/app-header";
 import BurgerIngredients from "../BurgerIngredients/burger-ingredients";
@@ -22,12 +17,9 @@ import ResetPassword from "../../pages/reset-password/reset-password";
 import History from "../../pages/history/history";
 import Profile from "../../pages/profile/profile";
 import Feed from "../../pages/feed/feed";
-import Modal from "../Modal/modal";
 import IngridientPage from "../../pages/ingridient-page/ingridient-page";
-import IngredientDetails from "../IngredientsDetails/ingredients-details";
 import { ProtectedRouteElement } from "../ProtectedRoute/protected-route";
-import { CLOSE_INFO_POPUP } from "../../services/actions/infoPopup";
-import IngredientDetailsPage from "../../pages/ingridientPage/ingridiet-page";
+
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
