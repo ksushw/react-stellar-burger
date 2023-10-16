61edf3df13b026d04240229801e3d23b48db5f33
ksushw <xenianevarenyh@yandex.ru>
2023-10-17 04:49:21 +0500

    edit header

--- src/components/AppHeader/app-header.jsx
+++ src/components/AppHeader/app-header.jsx
@@ -6,8 +6,16 @@ import {
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./app-header.module.css";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-export default function AppHeader() {
+export default function AppHeader({ constructor, feed, profile }) {
+  const [active, setActive] = useState("");
+
+  useEffect(() => {
+    setActive(window.location.pathname.split("/")[1] || "constructor");
+  }, []);
+  console.log(active);
   return (
     <header className={styles.header + " pt-4 pb-4 mt-10"} id="h">
       <div className={styles.container}>
@@ -15,29 +23,75 @@ export default function AppHeader() {
           to="/"
           href="#"
           className={
-            styles.link + " p-4 pl-5 pr-5 text_color_inactive " + styles.active
+            styles.link +
+            " p-4 pl-5 pr-5 text_color_inactive " +
+            (active === "constructor" ? styles.active : null)
           }
+          onClick={() => setActive("constructor")}
         >
-          <BurgerIcon type="secondary" />
+          <BurgerIcon
+            type={
+              (active === "constructor" ? styles.active : null)
+                ? "primary"
+                : "secondary"
+            }
+          />
           <p className="text text_type_main-default pl-2">Конструктор</p>
         </Link>
-        <Link to="/feed" className={styles.link + " p-4 pl-5 pr-5"}>
-          <ListIcon type="secondary" />
-          <p className="text text_type_main-default text_color_inactive pl-2">
+        <Link
+          to="/feed"
+          className={
+            styles.link +
+            " p-4 pl-5 pr-5 " +
+            (active === "feed" ? styles.active : null)
+          }
+          onClick={() => setActive("feed")}
+        >
+          <ListIcon
+            type={
+              (active === "feed" ? styles.active : null)
+                ? "primary"
+                : "secondary"
+            }
+          />
+          <p
+            className={
+              (active === "feed" ? styles.active : null) +
+              " text text_type_main-default text_color_inactive pl-2"
+            }
+          >
             Лента заказов
           </p>
         </Link>
-        <Link to="/">
+        <Link to="/" onClick={() => setActive("constructor")}>
           <div className={styles.logo}>
             <Logo />
           </div>
         </Link>
         <Link
           to="/profile"
-          className={styles.link + " p-4 pl-5 pr-5 " + styles.profile}
+          className={
+            styles.link +
+            " p-4 pl-5 pr-5  text_color_inactive " +
+            styles.profile +
+            " " +
+            (active === "profile" ? styles.active : null)
+          }
+          onClick={() => setActive("profile")}
         >
-          <ProfileIcon type="secondary" />
-          <p className="text text_type_main-default text_color_inactive pl-2">
+          <ProfileIcon
+            type={
+              (active === "profile" ? styles.active : null)
+                ? "primary"
+                : "secondary"
+            }
+          />
+          <p
+            className={
+              "text text_type_main-default pl-2 " +
+              (active === "profile" ? styles.active : null)
+            }
+          >
             Личный кабинет
           </p>
         </Link>
--- src/pages/ingridientPage/ingridiet-page.jsx
+++
@@ -1,5 +0,0 @@
-// import styles from "./ingredients-details.module.css";
-// import IngredientDetails from "../../components/IngredientsDetails/ingredients-details";
-// export default function IngredientDetailsPage({ children }) {
-//   return <>{item && <div>{children}</div>}</>;
-// }
